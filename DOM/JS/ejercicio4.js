a7dde623b7c6096fc93edb54afa68919b19b7a82
Feligx <feligxgamer@gmail.com>
2023-11-14 21:34:28 -0500

    [ADD] Ejercicio 4 start

---
+++ DOM/JS/ejercicio4.js
@@ -0,0 +1,35 @@
+const responses = [
+    {
+        name: "Name1",
+        last: "LastName1",
+        course: "Course1"
+    },
+    {
+        name: "Name3",
+        last: "LastName3",
+        course: "Course3"
+    },
+    {
+        name: "Name2",
+        last: "LastName2",
+        course: "Course2"
+    }
+]
+
+form = document.getElementById("courses")
+
+form.addEventListener(
+    "submit",
+    (e) => {
+        const data = Object.fromEntries(new FormData(e.target))
+
+        if (responses.some(e => {
+            return e.name === data.name && e.last === data.last && e.course === data.course
+        })) {
+            console.log("la respuesta es correcta")
+        } else {
+            console.log("la respuesta es incorrecta")
+        }
+        e.preventDefault()
+    }
+)
