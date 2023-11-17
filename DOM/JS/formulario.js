7e75cbcb183a71669f310a9f2b3a540b68a7edf2
anamarigarzon <anamari.garzon@urosario.edu.co>
2023-11-17 09:23:37 -0500

    update formulario

--- DOM/JS/formulario.js
+++ DOM/JS/formulario.js
@@ -1,50 +1,133 @@
-function validateDocument(document){
-    if (document === '') {
-        alert('Debe ingresar un correo');
-        return false;
-      }   
+const formulario = document.getElementById('formulario');
+const inputs = document.querySelectorAll('#formulario input');
+
+const expresiones = {
+	documento: /^[0-9]{5,}$/,
+    nombre:/^[a-zA-ZÀ-ÿ\s]{1,50}$/, 
+    apellido:/^[a-zA-ZÀ-ÿ\s]{1,50}$/, 
+    correo: /^[a-zA-Z0-9\_\-]+@misena\.edu\.co$/,
+    contrasena: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^a-zA-Z0-9])(?=.{10,})$/
 }
 
-function validateName(name){
-    if (name === '') {
-        alert('Debe ingresar un nombre');
-        return false;
-      }
+const campos = {
+	documento: false,
+    nombre: false, 
+    apellido: false, 
+    nacimiento: false,
+    correo: false,
+    contrasena: false,
+    valContrasena: false
 }
 
-function validateLastname(lastName){
-    if (lastName === '') {
-        alert('Debe ingresar sus apellidos');
-        return false;
-      }
+const validarFormulario = (e) => {
+	switch (e.target.name) {
+		case "documento":
+			validarCampo(expresiones.documento, e.target, 'documento');
+		break;
+		case "nombre":
+			validarCampo(expresiones.nombre, e.target, 'nombre');
+		break;
+		case "apellido":
+			validarCampo(expresiones.apellido, e.target, 'apellido');
+		break;
+        case "nacimiento":
+			validarEdad();
+		break;
+		case "correo":
+			validarCampo(expresiones.correo, e.target, 'correo');
+		break;
+
+		case "contrasena":
+			validarCampo(expresiones.contrasena, e.target, 'contrasena');
+		break;
+		case "valContraseña":
+			validarvalContrasena();
+		break;
+	}
 }
 
-function validateBirth(birth){
-    
+const validarCampo = (expresion, input, campo) => {
+	if(expresion.test(input.value)){
+		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto');
+		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-correcto');
+		document.querySelector(`#grupo__${campo} i`).classList.add('fa-check-circle');
+		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-times-circle');
+		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
+		campos[campo] = true;
+	} else {
+		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-incorrecto');
+		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-correcto');
+		document.querySelector(`#grupo__${campo} i`).classList.add('fa-times-circle');
+		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-check-circle');
+		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
+		campos[campo] = false;
+	}
 }
 
-function validateEmail(email){
-    if (email === '') {
-        alert('Debe ingresar un correo');
-        return false;
-      }   
+const validarvalContrasena = () => {
+	const inputContrasena = document.getElementById('contrasena');
+	const inputvalContrasena = document.getElementById('valContrasena');
+
+	if(inputContrasena.value !== inputvalContrasena.value){
+		document.getElementById(`grupo__valContrasena`).classList.add('formulario__grupo-incorrecto');
+		document.getElementById(`grupo__valContrasena`).classList.remove('formulario__grupo-correcto');
+		document.querySelector(`#grupo__valContrasena i`).classList.add('fa-times-circle');
+		document.querySelector(`#grupo__valContrasena i`).classList.remove('fa-check-circle');
+		document.querySelector(`#grupo__valContrasena .formulario__input-error`).classList.add('formulario__input-error-activo');
+		campos['valContrasena'] = false;
+	} else {
+		document.getElementById(`grupo__valContrasena`).classList.remove('formulario__grupo-incorrecto');
+		document.getElementById(`grupo__valContrasena`).classList.add('formulario__grupo-correcto');
+		document.querySelector(`#grupo__valContrasena i`).classList.remove('fa-times-circle');
+		document.querySelector(`#grupo__valContrasena i`).classList.add('fa-check-circle');
+		document.querySelector(`#grupo__valContrasena .formulario__input-error`).classList.remove('formulario__input-error-activo');
+		campos['valContrasena'] = true;
+	}
 }
 
-function validatePassword(password){
-    if (password === '') {
-        alert('Debe ingresar una contraseña');
-        return false;
-      }   
+const validarEdad = () => {
+    const inputNacimiento = document.getElementById('nacimiento');
+    const anioNacimiento = inputNacimiento.getFullYear();
+    const fechaActual = new Date();
+    const anioActual = fechaActual.getFullYear();
+    if(anioActual - anioNacimiento <= 18){
+		document.getElementById(`grupo__nacimiento`).classList.add('formulario__grupo-incorrecto');
+		document.getElementById(`grupo__nacimiento`).classList.remove('formulario__grupo-correcto');
+		document.querySelector(`#grupo__nacimiento i`).classList.add('fa-times-circle');
+		document.querySelector(`#grupo__nacimiento i`).classList.remove('fa-check-circle');
+		document.querySelector(`#grupo__nacimiento .formulario__input-error`).classList.add('formulario__input-error-activo');
+		campos['nacimiento'] = false;
+	} else {
+		document.getElementById(`grupo__nacimiento`).classList.remove('formulario__grupo-incorrecto');
+		document.getElementById(`grupo__nacimiento`).classList.add('formulario__grupo-correcto');
+		document.querySelector(`#grupo__nacimiento i`).classList.remove('fa-times-circle');
+		document.querySelector(`#grupo__nacimiento i`).classList.add('fa-check-circle');
+		document.querySelector(`#grupo__nacimiento .formulario__input-error`).classList.remove('formulario__input-error-activo');
+		campos['nacimiento'] = true;
+	}
 }
 
-function validateForm() {
-    // Obtener los valores de los campos
-    const tipo = document.querySelector('#tipoDocumento').value;
-    const documento = document.querySelector('#documento').value;
-    const nombre = document.querySelector('#nombre').value;
-    const apellido = document.querySelector('#apellido').value;
-    const nacimiento = document.querySelector('#nacimiento').value;
-    const correo = document.querySelector('#correo').value;
-    const contraseña = document.querySelector('#contraseña').value;
-    const valContraseña = document.querySelector('#valContraseña').value;
-}
+inputs.forEach((input) => {
+	input.addEventListener('keyup', validarFormulario);
+	input.addEventListener('blur', validarFormulario);
+});
+
+formulario.addEventListener('submit', (e) => {
+	e.preventDefault();
+
+	const terminos = document.getElementById('terminos');
+	if(campos.documento && campos.nombre && campos.apellido && campos.nacimiento && campos.correo && campos.contrasena && campos.correo && terminos.checked ){
+		formulario.reset();
+
+		document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
+		setTimeout(() => {
+			document.getElementById('formulario__mensaje-exito').classList.remove('formulario__mensaje-exito-activo');
+		}, 5000);
+
+		document.querySelectorAll('.formulario__grupo-correcto').forEach((icono) => {
+			icono.classList.remove('formulario__grupo-correcto');
+		});
+	} else {
+		document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
+	}
+});
